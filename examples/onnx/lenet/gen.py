ab5a2b0dfbb4944016ad46e113887d7c57fc2cd4
Georg Wiese <georgwiese@gmail.com>
2023-06-12 14:20:53 +0000

    Add lenet model

---
+++ examples/onnx/lenet/gen.py
@@ -0,0 +1,42 @@
+# Copied and adjusted from https://towardsdatascience.com/implementing-yann-lecuns-lenet-5-in-pytorch-5e05a0911320
+
+import torch.nn as nn
+
+from ezkl import export
+
+
+class LeNet5(nn.Module):
+
+    def __init__(self, n_classes):
+        super(LeNet5, self).__init__()
+        
+        self.feature_extractor = nn.Sequential(            
+            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1),
+            nn.Tanh(),
+            nn.AvgPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1),
+            nn.Tanh(),
+            nn.AvgPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5, stride=1),
+            nn.Tanh()
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=120, out_features=84),
+            nn.Tanh(),
+            nn.Linear(in_features=84, out_features=n_classes),
+        )
+
+
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        x = x.flatten(1)
+        logits = self.classifier(x)
+        return logits
+
+circuit = LeNet5(10)
+# TODO: Why 32 instead of 28?
+export(circuit, input_shape = [1,32,32])
+
+
+    
